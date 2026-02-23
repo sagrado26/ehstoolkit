39d53973b92312d7becdc69f144096c4ffff2660
sagrado26 <88437637+sagrado26@users.noreply.github.com>
2026-02-23 20:16:08 +0000

    commi

--- server/index.prod.ts
+++ server/index.prod.ts
@@ -7,50 +7,50 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  const start = Date.now();
-  const path = req.path;
-  let capturedJsonResponse: Record<string, any> | undefined = undefined;
+    const start = Date.now();
+    const path = req.path;
+    let capturedJsonResponse: Record<string, any> | undefined = undefined;
 
-  const originalResJson = res.json;
-  res.json = function (bodyJson, ...args) {
-    capturedJsonResponse = bodyJson;
-    return originalResJson.apply(res, [bodyJson, ...args]);
-  };
+    const originalResJson = res.json;
+    res.json = function (bodyJson, ...args) {
+        capturedJsonResponse = bodyJson;
+        return originalResJson.apply(res, [bodyJson, ...args]);
+    };
 
-  res.on("finish", () => {
-    const duration = Date.now() - start;
-    if (path.startsWith("/api")) {
-      let logLine = `${req.method} ${path} ${res.statusCode} in ${duration}ms`;
-      if (capturedJsonResponse) {
-        logLine += ` :: ${JSON.stringify(capturedJsonResponse)}`;
-      }
+    res.on("finish", () => {
+        const duration = Date.now() - start;
+        if (path.startsWith("/api")) {
+            let logLine = `${req.method} ${path} ${res.statusCode} in ${duration}ms`;
+            if (capturedJsonResponse) {
+                logLine += ` :: ${JSON.stringify(capturedJsonResponse)}`;
+            }
 
-      if (logLine.length > 80) {
-        logLine = logLine.slice(0, 79) + "…";
-      }
+            if (logLine.length > 80) {
+                logLine = logLine.slice(0, 79) + "…";
+            }
 
-      log(logLine);
-    }
-  });
+            log(logLine);
+        }
+    });
 
-  next();
+    next();
 });
 
 (async () => {
-  const server = await registerRoutes(app);
+    const server = await registerRoutes(app);
 
-  // In production, only serve static files
-  serveStatic(app);
+    // In production, only serve static files
+    serveStatic(app);
 
-  // ALWAYS serve the app on the port specified in the environment variable PORT
-  // Other ports are firewalled. Default to 3000 if not specified.
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '6666', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+    // ALWAYS serve the app on the port specified in the environment variable PORT
+    // Other ports are firewalled. Default to 3000 if not specified.
+    // this serves both the API and the client.
+    // It is the only port that is not firewalled.
+    const port = parseInt(process.env.PORT || '6666', 10);
+    server.listen({
+        port,
+        host: "0.0.0.0",
+    }, () => {
+        log(`serving on port ${port}`);
+    });
 })();
